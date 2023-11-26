25b95ba279dca463188937b6ab0f4a065413a74f
SungminPark <byulparan@gmail.com>
2023-11-26 19:57:12 +0900

    사운드 테스트 및 레이아웃 체크

--- src/main.js
+++ src/main.js
@@ -6,27 +6,33 @@ import 'p5';
 import './style.css';
 
 
-window.setup = () => {
-  createCanvas(400,400);
-}
-
-window.draw = () => {
-  background(0);
-  let time = millis() * 0.001;
-
-  noFill();
-  stroke(255);
-  translate(width*0.5, height*0.5);
-  ellipse(sin(time) * 100, cos(time) * 100, 30 + sin(time * 10) * 100, 30 + sin(time * 10) * 100);
-  
-}
-
-
-
-
 let initialized = false;
 let start = false;
 let csound;
+
+
+document.querySelector('#start').value = 'START - Please Use Headphone';
+
+
+window.setup = () => {
+  let canvas = createCanvas(300,300);
+  canvas.parent('canvas1');
+}
+
+window.draw = () => {
+  background(255);
+  if(start) {
+    let time = millis() * 0.001;
+
+    noFill();
+    stroke(0);
+    translate(width*0.5, height*0.5);
+    ellipse(sin(time) * 100, cos(time) * 100, 30 + sin(time * 30) * 100, 30 + sin(time * 30) * 100);
+  }  
+}
+
+
+
 
 async function init() {
   if(!initialized) {
@@ -42,10 +48,10 @@ document.querySelector('#start')
     await init();
     if(!start) {
       await csound.evalCode('schedule 1, 0, -1');
-      e.target.value = 'stop';
+      e.target.value = 'STOP';
     } else {
       await csound.evalCode('schedule "clock_off", 0, 0.1');
-      e.target.value = 'start';
+      e.target.value = 'START - Please Use Headphone';
     }
     start = !start;
   })
