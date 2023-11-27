bc33b74b7da5dba2b2e469b6432c216a3cbd644f
SungminPark <byulparan@gmail.com>
2023-11-28 01:18:29 +0900

    섹션별 나누기

--- src/csound.csd
+++ src/csound.csd
@@ -1,6 +1,6 @@
 <CsoundSynthesizer>
 <CsOptions>
--odac -m4 -i"dummy.wav"
+-odac -m4
 </CsOptions>
 <CsInstruments>
 
@@ -10,12 +10,39 @@ nchnls = 2
 0dbfs = 1.0
 seed 0
 
+
+
+;; ================================================================================
+;; GlobalSetup
+;; ================================================================================
 gkTempo init (60 / 60) * 32
+gkCount init 0
 gaRevL init 0
 gaRevR init 0
 
-
 schedule "reverb", 0, -1
+
+
+
+;; ================================================================================
+;; ControlChannel
+;; ================================================================================
+
+
+gkPadPan init 0
+gkPadPan chnexport "gkPadPan", 2, 0, i(gkPadPan)
+gkPadPanSpeed init 0.1
+gkPadPanSpeed chnexport "gkPadPanSpeed", 2, 0, i(gkPadPanSpeed)
+gkPadVol init 0
+gkPadVol chnexport "gkPadVol", 1, 0, i(gkPadVol)
+gkPadReverb init 0.0
+gkPadReverb chnexport "gkPadReverb", 1, 0, i(gkPadReverb)
+
+
+
+;; ================================================================================
+;; Synthesizer
+;; ================================================================================
 
 instr clock_off
   turnoff2 1, 0, 0
@@ -28,19 +55,36 @@ instr 1
     schedulek "schedule", .0, .1, kcount * 1/32
     kcount += 1
   endif
+  gkCount += 0.1 * gkPadPanSpeed
 endin
 
 instr schedule
   icount = p4
-  if icount % (1/4) == 0 then
-    schedule "_init_sound", .0, .25
+  if icount % (4) == 0 then
+    inotes[] fillarray 71,72,74,79
+    schedule "pad", .0, 6, inotes[random(0, lenarray(inotes))]
   endif
 endin
 
 
-instr _init_sound
-  asig oscil line:k(0.1, p3, 0.0), 440
-  outs asig, asig
+instr pad
+
+  kenv linseg 0,  p3*0.2, 1, p3*0.6,1, p3*0.2,0
+  a1 oscil 0.1, cpsmidinn(48)
+  a2 oscil 0.1, cpsmidinn(55)
+  a3 oscil 0.1, cpsmidinn(64)
+  a4 oscil 0.1, cpsmidinn(p4)
+  asig = a1 + a2 + a3 + a4
+  asig = asig*kenv*gkPadVol
+  kalpha = abs(gkCount % 360)
+  kbeta = 0
+  gkPadPan = kalpha
+  ; generate B format
+  aw, ax, ay, az, ar, as, at, au, av bformenc1 asig, kalpha, kbeta
+  al,ar bformdec1 1, aw, ax, ay, az, ar, as, at, au, av
+  outs al, ar
+  gaRevL += al * gkPadReverb * 0.3
+  gaRevR += ar * gkPadReverb * 0.3
 endin
 
 
--- src/main.js
+++ src/main.js
@@ -2,41 +2,23 @@
 
 import { Csound } from '@csound/browser';
 import csoundCode from './csound.csd?raw';
-import 'p5';
+import p5 from 'p5';
 import './style.css';
 
+
+// ================================================================================
+// initialization
+// ================================================================================
+
+document.querySelector('#start').value = 'START - Please Use Headphone';
 
 let initialized = false;
 let start = false;
 let csound;
 
-
-document.querySelector('#start').value = 'START - Please Use Headphone';
-
-
-window.setup = () => {
-  let canvas = createCanvas(300,300);
-  canvas.parent('canvas1');
-}
-
-window.draw = () => {
-  background(255);
-  if(start) {
-    let time = millis() * 0.001;
-
-    noFill();
-    stroke(0);
-    translate(width*0.5, height*0.5);
-    ellipse(sin(time) * 100, cos(time) * 100, 30 + sin(time * 30) * 100, 30 + sin(time * 30) * 100);
-  }  
-}
-
-
-
-
 async function init() {
   if(!initialized) {
-    csound = await Csound();
+    csound = await Csound({inputChannelCount: 0});
     await csound.compileCsdText(csoundCode);
     await csound.start();
     initialized = true;
@@ -55,5 +37,66 @@ document.querySelector('#start')
     }
     start = !start;
   })
+
+
+
+// ================================================================================
+// section.1
+// ================================================================================
+
+document.querySelector("#padvol")
+  .addEventListener("input", (e) => {
+    if(initialized) {
+      csound.setControlChannel("gkPadVol", e.target.value);
+    }
+  })
+
+document.querySelector("#padreverb")
+  .addEventListener("input", (e) => {
+    if(initialized) {
+      csound.setControlChannel("gkPadReverb", e.target.value);
+    }
+  })
+
+document.querySelector("#padPanSpeed")
+  .addEventListener("input", (e) => {
+    if(initialized) {
+      csound.setControlChannel("gkPadPanSpeed", e.target.value);
+    }
+  })
+
+
+
+let s1 = function( s ) {
+  s.setup = function() {
+    let parent = document.querySelector('#canvas1');
+    let canvas = s.createCanvas(parent.clientWidth, parent.clientHeight);
+    canvas.parent(parent);
+    s.angleMode(s.DEGREES);
+  }
+  let pan = 0.0;
+  s.draw = function() {
+    s.background(255);
+    if(start) {
+      csound.getControlChannel("gkPadPan").then(v => {
+	pan = v;
+      });
+      let time = s.millis() * 0.001;
+      s.noFill();
+      s.stroke(0);
+      s.push();
+      s.translate(s.width*0.5, s.height*0.5);
+      s.rotate(pan);
+      s.translate(0, 100);
+      s.ellipse(0, 0, 100,100);
+      s.pop();
+    }  
+  }
+};
+
+new p5(s1);
+
+
+
 
 
